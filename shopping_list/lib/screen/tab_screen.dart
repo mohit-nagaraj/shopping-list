4e66416b8dbbcdfdd8489559cb8cb77e9f889e42
Error-NaN <mohitnagaraj20@gmail.com>
2023-08-16 11:39:04 +0530

    update

--- shopping_list/lib/screen/tab_screen.dart
+++ shopping_list/lib/screen/tab_screen.dart
@@ -1,22 +1,90 @@
-import 'package:flutter/material.dart';
-import 'package:shopping_list/data/dummy_items.dart';
-import 'package:shopping_list/model/grocery_item.dart';
-import 'package:shopping_list/widget/display_item.dart';
+import 'dart:convert';
 
-class TabScreen extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:shopping_list/data/categories.dart';
+import 'package:shopping_list/model/category.dart';
+import 'package:shopping_list/model/grocery_item.dart';
+import 'package:shopping_list/screen/new_item_screen.dart';
+import 'package:shopping_list/widget/display_item.dart';
+import 'package:http/http.dart' as http;
+
+class TabScreen extends StatefulWidget {
   TabScreen({super.key});
-  final List<GroceryItem> items = groceryItems;
+
+  @override
+  State<TabScreen> createState() => _TabScreenState();
+}
+
+class _TabScreenState extends State<TabScreen> {
+  List<GroceryItem> items = [];
+
+  @override
+  void initState() {
+    loadItem();
+    super.initState();
+  }
+
+  void loadItem() async {
+    final url = Uri.https(
+        'test2-bb3f7-default-rtdb.europe-west1.firebasedatabase.app',
+        'shopping-list.json');
+    final response = await http.get(url);
+    final Map<String, dynamic> loadedData = json.decode(response.body);
+    final List<GroceryItem> tempList = [];
+    for (final item in loadedData.entries) {
+      final cat = categories.entries
+          .firstWhere((e) => e.value.name == item.value['category'])
+          .value;
+      tempList.add(
+        GroceryItem(
+          id: item.key,
+          category: cat,
+          name: item.value['name'],
+          quantity: item.value['quantity'],
+        ),
+      );
+    }
+
+    setState(() {
+      items = tempList;
+    });
+  }
+
+  void addItem() async {
+    await Navigator.push(
+        context, MaterialPageRoute(builder: (ctx) => NewItem()));
+    loadItem();
+  }
+
+  void removeItem(GroceryItem g) {
+    setState(() {
+      items.remove(g);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Grocery list'),
+        actions: [
+          IconButton(
+            onPressed: addItem,
+            icon: Icon(Icons.add),
+          )
+        ],
       ),
       body: Padding(
         padding: const EdgeInsets.only(top: 8.0),
-        child: ListView(
-          children: [...items.map((e) => DisplayItem(e))],
-        ),
+        child: (items.length == 0)
+            ? Center(
+                child: Text(
+                'Start adding some items',
+                style: TextStyle(fontSize: 16),
+              ))
+            : ListView(
+                children: [...items.map((e) => DisplayItem(e, removeItem))],
+              ),
       ),
     );
   }
--- shopping_list/lib/widget/display_item.dart
+++ shopping_list/lib/widget/display_item.dart
@@ -2,27 +2,42 @@ import 'package:flutter/material.dart';
 import 'package:shopping_list/model/grocery_item.dart';
 
 class DisplayItem extends StatelessWidget {
-  DisplayItem(this.item, {super.key});
+  DisplayItem(this.item, this.removeItem, {super.key});
+  void Function(GroceryItem g) removeItem;
   final GroceryItem item;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.symmetric(vertical: 10, horizontal: 24),
-      child: Row(
-        children: [
-          Container(
-            height: 15,
-            width: 15,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(4),
-              color: item.category.color,
+    return Dismissible(
+      key: ValueKey(item.id),
+      onDismissed: (direction) {
+        removeItem(item);
+      },
+      background: Container(
+        decoration: BoxDecoration(
+          color: Colors.red[400],
+        ),
+        child: Center(
+          child: Icon(Icons.delete_forever),
+        ),
+      ),
+      child: Container(
+        padding: EdgeInsets.symmetric(vertical: 10, horizontal: 24),
+        child: Row(
+          children: [
+            Container(
+              height: 15,
+              width: 15,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(4),
+                color: item.category.color,
+              ),
             ),
-          ),
-          SizedBox(width: 16),
-          Text(item.name),
-          Spacer(),
-          Text('${item.quantity}'),
-        ],
+            SizedBox(width: 16),
+            Text(item.name),
+            Spacer(),
+            Text('${item.quantity}'),
+          ],
+        ),
       ),
     );
   }
